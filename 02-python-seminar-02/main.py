edb6903dba84f3d37e6b0dd95c3a18f7389381bd
ioko <ikobozev3@gmail.com>
2024-01-31 14:59:41 +0300

    se02+

--- 02-python-seminar-02/main.py
+++ 02-python-seminar-02/main.py
@@ -1,1 +1,24 @@
-print("se-02")
+# найти факториал числа N
+N = 5
+result = 1
+while N > 0:
+    result = result * N
+    N -= 1
+print(result)
+
+# Каким по счёту числом Фибоначчи является число N, если не является, то вывести -1
+f0 = 0
+f1 = 1
+fib = 1
+N = 6
+count = 1
+while (fib < N):
+    fib = f0 + f1
+    f0 = f1
+    f1 = fib
+    count += 1
+print(fib)
+if fib == N:
+    print(count)
+else:
+    print(-1)
